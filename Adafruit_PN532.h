fdcf83ba1c92a0ee5929c4d1e504da14756c06e6
A. van der Pluijm <info@urbanlink.nl>
2017-01-03 12:39:25 +0100

    remove spi
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- Adafruit_PN532.h
+++ Adafruit_PN532.h
@@ -1,16 +1,44 @@
 /**************************************************************************/
-/*!
-    @file     PN532.h
-    @author   Adafruit Industries & Seeed Studio
-    @license  BSD
+/*! 
+    @file     Adafruit_PN532.h
+    @author   Adafruit Industries
+	@license  BSD (see license.txt)
+	
+
+	This is a library for the Adafruit PN532 NFC/RFID breakout boards
+	This library works with the Adafruit NFC breakout 
+	----> https://www.adafruit.com/products/364
+	
+	Check out the links above for our tutorials and wiring diagrams 
+  These chips use SPI or I2C to communicate.
+	
+	Adafruit invests time and resources providing this open source code, 
+	please support Adafruit and open-source hardware by purchasing 
+	products from Adafruit!
+
+	@section  HISTORY
+
+  v2.0  - Refactored to add I2C support from Adafruit_NFCShield_I2C library.
+
+	v1.1  - Added full command list
+          - Added 'verbose' mode flag to constructor to toggle debug output
+          - Changed readPassiveTargetID() to return variable length values
+	
 */
 /**************************************************************************/
 
-#ifndef __PN532_H__
-#define __PN532_H__
+#ifndef ADAFRUIT_PN532_H
+#define ADAFRUIT_PN532_H
 
-#include <stdint.h>
-#include "PN532Interface.h"
+#include "Arduino.h"
+
+#define PN532_PREAMBLE                      (0x00)
+#define PN532_STARTCODE1                    (0x00)
+#define PN532_STARTCODE2                    (0xFF)
+#define PN532_POSTAMBLE                     (0x00)
+
+#define PN532_HOSTTOPN532                   (0xD4)
+#define PN532_PN532TOHOST                   (0xD5)
 
 // PN532 Commands
 #define PN532_COMMAND_DIAGNOSE              (0x00)
@@ -49,6 +77,18 @@
 #define PN532_RESPONSE_INDATAEXCHANGE       (0x41)
 #define PN532_RESPONSE_INLISTPASSIVETARGET  (0x4B)
 
+#define PN532_WAKEUP                        (0x55)
+
+#define PN532_SPI_STATREAD                  (0x02)
+#define PN532_SPI_DATAWRITE                 (0x01)
+#define PN532_SPI_DATAREAD                  (0x03)
+#define PN532_SPI_READY                     (0x01)
+
+#define PN532_I2C_ADDRESS                   (0x48 >> 1)
+#define PN532_I2C_READBIT                   (0x01)
+#define PN532_I2C_BUSY                      (0x00)
+#define PN532_I2C_READY                     (0x01)
+#define PN532_I2C_READYTIMEOUT              (20)
 
 #define PN532_MIFARE_ISO14443A              (0x00)
 
@@ -57,19 +97,11 @@
 #define MIFARE_CMD_AUTH_B                   (0x61)
 #define MIFARE_CMD_READ                     (0x30)
 #define MIFARE_CMD_WRITE                    (0xA0)
-#define MIFARE_CMD_WRITE_ULTRALIGHT         (0xA2)
 #define MIFARE_CMD_TRANSFER                 (0xB0)
 #define MIFARE_CMD_DECREMENT                (0xC0)
 #define MIFARE_CMD_INCREMENT                (0xC1)
 #define MIFARE_CMD_STORE                    (0xC2)
-
-// FeliCa Commands
-#define FELICA_CMD_POLLING                  (0x00)
-#define FELICA_CMD_REQUEST_SERVICE          (0x02)
-#define FELICA_CMD_REQUEST_RESPONSE         (0x04)
-#define FELICA_CMD_READ_WITHOUT_ENCRYPTION  (0x06)
-#define FELICA_CMD_WRITE_WITHOUT_ENCRYPTION (0x08)
-#define FELICA_CMD_REQUEST_SYSTEM_CODE      (0x0C)
+#define MIFARE_ULTRALIGHT_CMD_WRITE         (0xA2)
 
 // Prefixes for NDEF Records (to identify record type)
 #define NDEF_URIPREFIX_NONE                 (0x00)
@@ -117,92 +149,64 @@
 #define PN532_GPIO_P34                      (4)
 #define PN532_GPIO_P35                      (5)
 
-// FeliCa consts
-#define FELICA_READ_MAX_SERVICE_NUM         16
-#define FELICA_READ_MAX_BLOCK_NUM           12 // for typical FeliCa card
-#define FELICA_WRITE_MAX_SERVICE_NUM        16
-#define FELICA_WRITE_MAX_BLOCK_NUM          10 // for typical FeliCa card
-#define FELICA_REQ_SERVICE_MAX_NODE_NUM     32
+class Adafruit_PN532{
+ public:
+  Adafruit_PN532(uint8_t irq, uint8_t reset);  // Hardware I2C
+  void begin(void);
+  
+  // Generic PN532 functions
+  bool     SAMConfig(void);
+  uint32_t getFirmwareVersion(void);
+  bool     sendCommandCheckAck(uint8_t *cmd, uint8_t cmdlen, uint16_t timeout = 1000);  
+  bool     writeGPIO(uint8_t pinstate);
+  uint8_t  readGPIO(void);
+  bool     setPassiveActivationRetries(uint8_t maxRetries);
+  
+  // ISO14443A functions
+  bool readPassiveTargetID(uint8_t cardbaudrate, uint8_t * uid, uint8_t * uidLength, uint16_t timeout = 0); //timeout 0 means no timeout - will block forever.
+  bool inDataExchange(uint8_t * send, uint8_t sendLength, uint8_t * response, uint8_t * responseLength);
+  bool inListPassiveTarget();
+  
+  // Mifare Classic functions
+  bool    mifareclassic_IsFirstBlock (uint32_t uiBlock);
+  bool    mifareclassic_IsTrailerBlock (uint32_t uiBlock);
+  uint8_t mifareclassic_AuthenticateBlock (uint8_t * uid, uint8_t uidLen, uint32_t blockNumber, uint8_t keyNumber, uint8_t * keyData);
+  uint8_t mifareclassic_ReadDataBlock (uint8_t blockNumber, uint8_t * data);
+  uint8_t mifareclassic_WriteDataBlock (uint8_t blockNumber, uint8_t * data);
+  uint8_t mifareclassic_FormatNDEF (void);
+  uint8_t mifareclassic_WriteNDEFURI (uint8_t sectorNumber, uint8_t uriIdentifier, const char * url);
+  
+  // Mifare Ultralight functions
+  uint8_t mifareultralight_ReadPage (uint8_t page, uint8_t * buffer);
+  uint8_t mifareultralight_WritePage (uint8_t page, uint8_t * data);
 
-class PN532
-{
-public:
-    PN532(PN532Interface &interface);
+  // NTAG2xx functions
+  uint8_t ntag2xx_ReadPage (uint8_t page, uint8_t * buffer);
+  uint8_t ntag2xx_WritePage (uint8_t page, uint8_t * data);
+  uint8_t ntag2xx_WriteNDEFURI (uint8_t uriIdentifier, char * url, uint8_t dataLen);
+  
+  // Help functions to display formatted text
+  static void PrintHex(const byte * data, const uint32_t numBytes);
+  static void PrintHexChar(const byte * pbtData, const uint32_t numBytes);
 
-    void begin(void);
+ private:
+  uint8_t _ss, _clk, _mosi, _miso;
+  uint8_t _irq, _reset;
+  uint8_t _uid[7];       // ISO14443A uid
+  uint8_t _uidLen;       // uid len
+  uint8_t _key[6];       // Mifare Classic key
+  uint8_t _inListedTag;  // Tg number of inlisted tag.
+  bool    _usingSPI;     // True if using SPI, false if using I2C.
+  bool    _hardwareSPI;  // True is using hardware SPI, false if using software SPI.
 
-    // Generic PN532 functions
-    bool SAMConfig(void);
-    uint32_t getFirmwareVersion(void);
-    uint32_t readRegister(uint16_t reg);
-    uint32_t writeRegister(uint16_t reg, uint8_t val);
-    bool writeGPIO(uint8_t pinstate);
-    uint8_t readGPIO(void);
-    bool setPassiveActivationRetries(uint8_t maxRetries);
+  // Low level communication functions that handle both SPI and I2C.
+  void readdata(uint8_t* buff, uint8_t n);
+  void writecommand(uint8_t* cmd, uint8_t cmdlen);
+  bool isready();
+  bool waitready(uint16_t timeout);
+  bool readack();
 
-    /**
-    * @brief    Init PN532 as a target
-    * @param    timeout max time to wait, 0 means no timeout
-    * @return   > 0     success
-    *           = 0     timeout
-    *           < 0     failed
-    */
-    int8_t tgInitAsTarget(uint16_t timeout = 0);
-    int8_t tgInitAsTarget(const uint8_t* command, const uint8_t len, const uint16_t timeout = 0);
-
-    int16_t tgGetData(uint8_t *buf, uint8_t len);
-    bool tgSetData(const uint8_t *header, uint8_t hlen, const uint8_t *body = 0, uint8_t blen = 0);
-
-    int16_t inRelease(const uint8_t relevantTarget = 0);
-
-    // ISO14443A functions
-    bool inListPassiveTarget();
-    bool readPassiveTargetID(uint8_t cardbaudrate, uint8_t *uid, uint8_t *uidLength, uint16_t timeout = 1000);
-    bool inDataExchange(uint8_t *send, uint8_t sendLength, uint8_t *response, uint8_t *responseLength);
-
-    // Mifare Classic functions
-    bool mifareclassic_IsFirstBlock (uint32_t uiBlock);
-    bool mifareclassic_IsTrailerBlock (uint32_t uiBlock);
-    uint8_t mifareclassic_AuthenticateBlock (uint8_t *uid, uint8_t uidLen, uint32_t blockNumber, uint8_t keyNumber, uint8_t *keyData);
-    uint8_t mifareclassic_ReadDataBlock (uint8_t blockNumber, uint8_t *data);
-    uint8_t mifareclassic_WriteDataBlock (uint8_t blockNumber, uint8_t *data);
-    uint8_t mifareclassic_FormatNDEF (void);
-    uint8_t mifareclassic_WriteNDEFURI (uint8_t sectorNumber, uint8_t uriIdentifier, const char *url);
-
-    // Mifare Ultralight functions
-    uint8_t mifareultralight_ReadPage (uint8_t page, uint8_t *buffer);
-    uint8_t mifareultralight_WritePage (uint8_t page, uint8_t *buffer);
-
-    // FeliCa Functions
-    int8_t felica_Polling(uint16_t systemCode, uint8_t requestCode, uint8_t *idm, uint8_t *pmm, uint16_t *systemCodeResponse, uint16_t timeout=1000);
-    int8_t felica_SendCommand (const uint8_t * command, uint8_t commandlength, uint8_t * response, uint8_t * responseLength);
-    int8_t felica_RequestService(uint8_t numNode, uint16_t *nodeCodeList, uint16_t *keyVersions) ;
-    int8_t felica_RequestResponse(uint8_t *mode);
-    int8_t felica_ReadWithoutEncryption (uint8_t numService, const uint16_t *serviceCodeList, uint8_t numBlock, const uint16_t *blockList, uint8_t blockData[][16]);
-    int8_t felica_WriteWithoutEncryption (uint8_t numService, const uint16_t *serviceCodeList, uint8_t numBlock, const uint16_t *blockList, uint8_t blockData[][16]);
-    int8_t felica_RequestSystemCode(uint8_t *numSystemCode, uint16_t *systemCodeList);
-    int8_t felica_Release();
-
-    // Help functions to display formatted text
-    static void PrintHex(const uint8_t *data, const uint32_t numBytes);
-    static void PrintHexChar(const uint8_t *pbtData, const uint32_t numBytes);
-
-    uint8_t *getBuffer(uint8_t *len) {
-        *len = sizeof(pn532_packetbuffer) - 4;
-        return pn532_packetbuffer;
-    };
-
-private:
-    uint8_t _uid[7];  // ISO14443A uid
-    uint8_t _uidLen;  // uid len
-    uint8_t _key[6];  // Mifare Classic key
-    uint8_t inListedTag; // Tg number of inlisted tag.
-    uint8_t _felicaIDm[8]; // FeliCa IDm (NFCID2)
-    uint8_t _felicaPMm[8]; // FeliCa PMm (PAD)
-
-    uint8_t pn532_packetbuffer[255];
-
-    PN532Interface *_interface;
+  // Note there are i2c_read and i2c_write inline functions defined in the .cpp file.
 };
 
 #endif
--- membership_card.ino
+++ membership_card.ino
@@ -1,5 +1,9 @@
 /**************************************************************************/
 /*! 
+    @file     iso14443a_uid.pde
+    @author   Adafruit Industries
+    @license  BSD (see license.txt)
+
     This example will attempt to connect to an ISO14443A
     card or tag and retrieve some basic information about it
     that can be used to determine what type of card it is.   
@@ -7,34 +11,29 @@
     Note that you need the baud rate to be 115200 because we need to print
     out the data and read from the card at the same time!
 
-    To enable debug message, define DEBUG in PN532/PN532_debug.h
+    This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+    This library works with the Adafruit NFC breakout 
+      ----> https://www.adafruit.com/products/364
+     
+    Check out the links above for our tutorials and wiring diagrams 
+    These chips use SPI or I2C to communicate.
     
+    Adafruit invests time and resources providing this open source code, 
+    please support Adafruit and open-source hardware by purchasing 
+    products from Adafruit!
+
 */
 /**************************************************************************/
 
-#if 0
-  #include <SPI.h>
-  #include <PN532_SPI.h>
-  #include "PN532.h"
+#include <Wire.h>
+#include "Adafruit_PN532.h"
 
-  PN532_SPI pn532spi(SPI, 10);
-  PN532 nfc(pn532spi);
-#elif 0
-  #include <PN532_HSU.h>
-  #include <PN532.h>
-      
-  PN532_HSU pn532hsu(Serial1);
-  PN532 nfc(pn532hsu);
-#else 
-  #include <Wire.h>
-  #include <PN532_I2C.h>
-  #include <PN532.h>
-  #include <NfcAdapter.h>
-  
-  PN532_I2C pn532i2c(Wire);
-  PN532 nfc(pn532i2c);
-#endif
-  
+#define PN532_IRQ   (14)
+#define PN532_RESET (12)
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+
 void setup(void) {
   Serial.begin(115200);
   Serial.println("Hello!");
@@ -59,14 +58,14 @@ void setup(void) {
   
   // configure board to read RFID tags
   nfc.SAMConfig();
-    
+  
   Serial.println("Waiting for an ISO14443A card");
 }
 
 void loop(void) {
   boolean success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
   
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
@@ -77,16 +76,13 @@ void loop(void) {
     Serial.println("Found a card!");
     Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("UID Value: ");
-    for (uint8_t i=0; i < uidLength; i++) 
-    {
+    for (uint8_t i=0; i < uidLength; i++) {
       Serial.print(" 0x");Serial.print(uid[i], HEX); 
     }
     Serial.println("");
     // Wait 1 second before continuing
     delay(1000);
-  }
-  else
-  {
+  } else {
     // PN532 probably timed out waiting for a card
     Serial.println("Timed out waiting for a card");
   }
